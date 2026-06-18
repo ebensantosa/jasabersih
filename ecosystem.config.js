51229f8d8d347c4d8b04f5cd1a331b005c1ffb58
Eben Santosa <ebensantosa9@gmail.com>
2026-06-18 10:30:18 +0700

    fix admin pm2 next binary path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       name: 'jasabersih-admin',
       cwd: '/var/www/jasabersih/apps/admin',
-      script: 'node_modules/next/dist/bin/next',
+      script: '../../node_modules/next/dist/bin/next',
       args: 'start -p 3000',
       instances: 1,
       exec_mode: 'fork',
