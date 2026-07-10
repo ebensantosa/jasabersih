c56353e2ab02f088e4286cce5497cfb72ae2325d
Eben Santosa <ebensantosa9@gmail.com>
2026-07-10 13:48:34 +0700

    chore: bump mobile version to 1.6.1 (versionCode 27)

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -131,7 +131,7 @@ module.exports = {
   expo: {
     name: 'JasaBersih',
     slug: 'jasabersih-app',
-    version: '1.6.0',
+    version: '1.6.1',
     scheme: 'jasabersih',
     orientation: 'default',
     userInterfaceStyle: 'light',
@@ -151,7 +151,7 @@ module.exports = {
     },
     android: {
       package: 'com.jasabersih.app',
-      versionCode: 26,
+      versionCode: 27,
       edgeToEdgeEnabled: false,
       permissions: [
         'FOREGROUND_SERVICE',
